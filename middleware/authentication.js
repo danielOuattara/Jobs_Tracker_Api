f0758465fb9ef990e51d73ff6f183ecaa9ef9da1
danielOuattara <daniel.ouattara@gmx.com>
2022-11-17 22:46:21 +0100

    middlewares update

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -7,15 +7,15 @@ const auth = async (req, res, next) => {
   if (!authHeader || !authHeader.startsWith("Bearer")) {
     throw new UnauthenticatedError("Request Denied !");
   }
-  const token = authHeader.split(" ")[1];
 
   try {
+    const token = authHeader.split(" ")[1];
     const payload = jwt.verify(token, process.env.JWT_SECRET);
 
     //--> Less: created local user object: only userId & name
     // req.user = { userId: payload.userId, name: payload.name };
 
-    // --> Better: register in "req" a complete Mongoose user object 
+    // --> Better: register in "req" a complete Mongoose user object
     //             with all possible associations.
     const user = await User.findById(payload.userId).select("-password");
     req.user = user;
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,24 +1,21 @@
+//mdn Object.keys
 //-----------------------------------------------------------------------
 
-// const { CustomAPIError } = require("./../errors");
-// const { StatusCodes } = require("http-status-codes");
+/* 
+In this function we create a FINAL error object called customError.
+We populate properties of this error object according to the error origin:
 
-// const errorHandlerMiddleware = (err, req, res, next) => {
-//   if (err instanceof CustomAPIError) {
-//     return res.status(err.statusCode).json({ ErrorMessage: err.message });
-//   }
-//   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
-// };
-
-// module.exports = errorHandlerMiddleware;
-
-//-------------------------------------------------------------------------
+- our custom error (from controllers logic)
+- mongoose/mongodb validation/cast/duplicate error ( resolved here)
+- others error ( from anywhere else)
+*/
 
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  // --> set default
-  let customError = {
+  //
+  /* --> set default */
+  const customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     message: err.message || "Something wrong, try again later",
   };
@@ -28,21 +25,20 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       .map((item) => item.message)
       .join(" ");
     customError.statusCode = 400;
-  }
-
-  if (err.code && err.code === 11000) {
-    customError.message = `Duplicate ${Object.keys(
-      err.keyValue
-    )}: ${Object.values(err.keyValue)} Please, choose another one !`;
+  } else if (err.code && err.code === 11000) {
+    customError.message = `Duplicate ${Object.keys(err.keyValue)[0]}: ${
+      Object.values(err.keyValue)[0]
+    } Please, choose another one !`;
     customError.statusCode = 400;
-  }
-
-  if(err.name === "CastError") {
-    customError.message = `Not item using Id: ${err.value}`
+  } else if (err.name === "CastError") {
+    customError.message = `No item using Id: ${err.value}`;
     customError.statusCode = 404;
   }
-  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
-  return res.status(customError.statusCode).json({ msg: customError.message });
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+  return res
+    .status(customError.statusCode)
+    .json({ message: customError.message });
 };
 
 module.exports = errorHandlerMiddleware;
