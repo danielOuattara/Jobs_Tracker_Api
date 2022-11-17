62aac1596fcb66097dcad766d108988c43977654
danielOuattara <daniel.ouattara@gmx.com>
2022-11-17 22:43:49 +0100

    update db to mpngoose 6+ requirements

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose");
 
-const connectParams = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-};
-
-const connectDB = (url) => mongoose.connect(url, connectParams);
+const connectDB = (url) => mongoose.connect(url);
 
 module.exports = connectDB;
